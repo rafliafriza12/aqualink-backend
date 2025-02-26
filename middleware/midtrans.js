a74c8eb8dcceeda215412fa7e8006fcf536beab2
rafliafriza12 <rafliafriza90@gmail.com>
2025-02-26 22:12:00 +0700

    fix: remove credential data

--- middleware/midtrans.js
+++ middleware/midtrans.js
@@ -1,14 +1,9 @@
 import Midtrans from "midtrans-client";
 
 const midtransClient = new Midtrans.Snap({
-  isProduction: true, // Ubah ke true jika di production
-  serverKey: "Mid-server-Whs2-Z-Jp1V3ZURCnJRvAfn8",
-  clientKey: "Mid-client-PNhSygI_tx1xDs_b",
+  isProduction: process.env.IS_PRODUCTION === "true", // Ubah ke true jika di production
+  serverKey: process.env.MIDTRANS_SERVER_KEY,
+  clientKey: process.env.MIDTRANS_CLIENT_KEY,
 });
-// const midtransClient = new Midtrans.CoreApi({
-//   isProduction: false, // Ubah ke true jika di production
-//   serverKey: "SB-Mid-server-Fj3ePUi0ZtXwRwemdjNnshf-",
-//   clientKey: "SB-Mid-client-63GqcLJxWhrc5D0A",
-// });
 
 export default midtransClient;
